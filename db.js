dc32ef09929d911b342caf34a50aba166b73873c
0x800f081ff <kunadhc@gmail.com>
2025-10-05 18:19:24 -0400

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
-// Create a connection pool to your InfinityFree database
 const pool = mysql.createPool({
-  host: 'sql312.infinityfree.com',    // your DB host
-  user: 'if0_40093677',               // your DB username
-  password: 'f4H784tvZuL2',           // your DB password
-  database: 'if0_40093677_keyauth',   // your DB name
+  host: 'caboose.proxy.rlwy.net',     // Railway host
+  user: 'root',                        // Railway username
+  password: 'jIpgGOwptNDwzkJTxdQsdlvBQjUQbOWM',  // Railway password
+  database: 'railway',                 // Railway database name
+  port: 30323,                         // Railway port
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
