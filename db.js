4f39cac087c4c18f4ae6719dd1e1dfa46e681169
0x800f081ff <kunadhc@gmail.com>
2025-10-05 20:10:39 -0400

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
+const mysql = require('mysql2/promise');
+
 const pool = mysql.createPool({
   host: process.env.host,
   user: process.env.user,
   password: process.env.password,
   database: process.env.database,
-  port: process.env.port ? parseInt(process.env.port) : 3306,
+  port: parseInt(process.env.port),
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
+
+module.exports = pool;
